f33a5b41475c9c0764e3cd8d626ddad8512950f5
Mihailo Cvetkovic <info@mihailo.cz>
2025-07-16 14:18:51 +0200

    utm

--- components/made-by-aristoclick.tsx
+++ components/made-by-aristoclick.tsx
@@ -2,7 +2,7 @@ import SvgMadeByAristoclick from '@/components/svg/svg-made-by-aristoclick';
 
 export default function MadeByAristoclick() {
     return (
-        <a href="https://www.aristoclick.cz?utm_source=XYZ&utm_medium=footer&utm_campaign=made-by-aristoclick"
+        <a href="https://www.aristoclick.cz?utm_source=website&utm_medium=XYZ&utm_campaign=made-by-aristoclick"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-flex w-full max-w-[136px] hover:opacity-70 transition-opacity duration-300"
